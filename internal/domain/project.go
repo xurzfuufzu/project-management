9967dab8cc01c9e4747115f236ec2594c7722a4c
xurzfuufzu <joodikstyle@gmail.com>
2024-08-21 23:11:01 +0500

    change data type

--- internal/domain/project.go
+++ internal/domain/project.go
@@ -1,10 +1,12 @@
 package domain
 
+import "time"
+
 type Project struct {
-	ID          string `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	StartDate   string `json:"start_date"`
-	EndDate     string `json:"end_date"`
-	ManagerID   string `json:"manager_id"`
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     time.Time `json:"end_date"`
+	ManagerID   string    `json:"manager_id"`
 }
--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,9 +1,11 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID               string `json:"id"`
-	Name             string `json:"name"`
-	Email            string `json:"email"`
-	RegistrationDate string `json:"registration_date"`
-	Role             string `json:"role"`
+	ID               string    `json:"id"`
+	Name             string    `json:"name"`
+	Email            string    `json:"email"`
+	RegistrationDate time.Time `db:"registration_date"`
+	Role             string    `json:"role"`
 }
